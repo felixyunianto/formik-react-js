80ea4c62eaed1a8d071c502c651c504a198c4aac
felixyunianto <felixyunianto@gmail.com>
2021-04-11 16:02:50 +0700

    4. Validation function and Displaying error messages

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -2,24 +2,45 @@ import React from "react";
 import { useFormik } from "formik";
 
 const InputForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      fullname: "",
+    },
+    onSubmit: (values) => {
+      console.log("Formik values ", values);
+    },
+    validate: (values) => {
+      let errors = {};
 
-    const formik = useFormik({
-        initialValues : {
-            username : '',
-            email : '',
-            fullname : ''
-        },
-        onSubmit : values => {
-          console.log("Formik values ", values);
-        }
-    })
+      if (!values.username) {
+        errors.username = "Required";
+      }
 
-    
+      if (!values.email) {
+        errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email format";
+      }
+
+      if (!values.fullname) {
+        errors.fullname = "Required";
+      }
+
+      return errors;
+    },
+  });
 
   return (
     <div className="h-screen flex bg-gray-100 justify-center items-center">
       <div className="w-full max-w-xs">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={formik.handleSubmit}>
+        <form
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={formik.handleSubmit}
+        >
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -36,6 +57,11 @@ const InputForm = () => {
               onChange={formik.handleChange}
               value={formik.values.username}
             />
+            {formik.errors.username ? (
+              <span className="text-red-500 text-sm">
+                {formik.errors.username}
+              </span>
+            ) : null}
           </div>
           <div className="mb-4">
             <label
@@ -53,6 +79,11 @@ const InputForm = () => {
               onChange={formik.handleChange}
               value={formik.values.email}
             />
+            {formik.errors.email ? (
+              <span className="text-red-500 text-sm">
+                {formik.errors.email}
+              </span>
+            ) : null}
           </div>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">
@@ -67,10 +98,20 @@ const InputForm = () => {
               onChange={formik.handleChange}
               value={formik.values.fullname}
             />
+            {formik.errors.fullname ? (
+              <span className="text-red-500 text-sm">
+                {formik.errors.fullname}
+              </span>
+            ) : null}
           </div>
           <div className="mb-4">
-              <button type="submit" className="w-full py-2 bg-blue-500 text-white rounded">Submit</button>
-            </div> 
+            <button
+              type="submit"
+              className="w-full py-2 bg-blue-500 text-white rounded"
+            >
+              Submit
+            </button>
+          </div>
         </form>
       </div>
     </div>
